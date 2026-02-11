0628880c1e87e16d79f7794721b7010d517f59c7
Leo Macedo <leomacedo19@gmail.com>
2026-02-11 12:05:34 -0300

    feat: Questão 2 da lista C

--- Extras/Lógica_com_JS(C).py
+++ Extras/Lógica_com_JS(C).py
@@ -4,6 +4,7 @@ while continuar:
     print("")
     print("Aplicando Lógica com JS(C) - Terceira lista")
     print("1 - Ler 10 numeros inteiros e exibir a soma de todos")
+    print("2 - Formulário de aprovação de aluno")
     print("")
     opcao = input("Qual resposta quer ver? Digite o numero: ")
     print("")
@@ -15,6 +16,19 @@ while continuar:
                 numero = int(input(f"Digite o {i+1}º número inteiro: "))
                 soma += numero
             print(f"A soma dos números é: {soma}")
+
+        case "2":
+            nome = input("Digite o nome do aluno: ")
+            nota1 = float(input("Digite a primeira nota: "))
+            nota2 = float(input("Digite a segunda nota: "))
+            media = (nota1 + nota2) / 2
+            print("")
+            print(f"Aluno: {nome}")
+            print(f"Média: {media}")
+            if media >= 6:
+                print(f"Situação: Aprovado")
+            else:
+                print(f"Situação: Reprovado")
 
         case _:
             print("Opção inválida.")
