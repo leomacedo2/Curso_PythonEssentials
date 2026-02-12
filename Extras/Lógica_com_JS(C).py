b31803dbbc7fe90c6c26207c0caa24a327b59519
Leo Macedo <leomacedo19@gmail.com>
2026-02-12 10:19:25 -0300

    feat: Questão 3 Lista C

--- Extras/Lógica_com_JS(C).py
+++ Extras/Lógica_com_JS(C).py
@@ -5,6 +5,7 @@ while continuar:
     print("Aplicando Lógica com JS(C) - Terceira lista")
     print("1 - Ler 10 numeros inteiros e exibir a soma de todos")
     print("2 - Formulário de aprovação de aluno")
+    print("3 - Promoção de salário")
     print("")
     opcao = input("Qual resposta quer ver? Digite o numero: ")
     print("")
@@ -29,6 +30,20 @@ while continuar:
                 print(f"Situação: Aprovado")
             else:
                 print(f"Situação: Reprovado")
+
+        
+        case "3":
+            salario_atual = float(input("Digite o salário atual: "))
+            if salario_atual < 1500:
+                aumento = salario_atual * 0.50
+            elif 1500 <= salario_atual < 4300:
+                aumento = salario_atual * 0.40
+            else:
+                aumento = salario_atual * 0.30
+            salario_novo = salario_atual + aumento
+            print(f"Salário atual: R${salario_atual:.2f}")
+            print(f"Aumento: R${aumento:.2f}")
+            print(f"Salário novo: R${salario_novo:.2f}")
 
         case _:
             print("Opção inválida.")
