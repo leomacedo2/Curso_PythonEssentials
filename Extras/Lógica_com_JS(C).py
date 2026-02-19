caa58631adf7e80b57bd6aa55f3953841a7bd47c
Leo Macedo <leomacedo19@gmail.com>
2026-02-19 07:40:36 -0300

    feat: Questão 7 Lista C

--- Extras/Lógica_com_JS(C).py
+++ Extras/Lógica_com_JS(C).py
@@ -9,6 +9,7 @@ while continuar:
     print("4 - Idade de um nadador e definir a categoria")
     print("5 - Refrigerantes Meia-Cola")
     print("6 - Calculo de média de notas de alunos")
+    print("7 - Fórmula de Bhaskara")
     print("")
     opcao = input("Qual resposta quer ver? Digite o numero: ")
     print("")
@@ -89,7 +90,23 @@ while continuar:
             else:
                 print(f"A média é {media:.2f}. Aluno reprovado.")
                 print("Infelizmente, não recebeu o certificado de aprovação.")
-                
+        
+
+        case "7":
+            a = float(input("Digite o valor de a: "))
+            b = float(input("Digite o valor de b: "))
+            c = float(input("Digite o valor de c: "))
+            delta = b**2 - 4*a*c
+            if delta < 0:
+                print("A equação não possui raízes reais.")
+            elif delta == 0:
+                raiz = -b / (2*a)
+                print(f"A equação possui uma raiz real: {raiz:.2f}")
+            else:
+                raiz1 = (-b + delta**0.5) / (2*a)
+                raiz2 = (-b - delta**0.5) / (2*a)
+                print(f"A equação possui duas raízes reais: {raiz1:.2f} e {raiz2:.2f}")           
+
 
         case _:
             print("Opção inválida.")
