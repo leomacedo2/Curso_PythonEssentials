f6c1a1f6708dfe18c7578ceb08e51fb1a4630ff4
Leo Macedo <leomacedo19@gmail.com>
2026-02-23 10:40:27 -0300

    feat: Questão 2 da Lista

--- Extras/Lista_de_Exercícios_Algoritmo.py
+++ Extras/Lista_de_Exercícios_Algoritmo.py
@@ -4,6 +4,7 @@ while continuar:
     print("")
     print("Lista de Exercícios - Algoritmo")
     print("1 - Atribuição de variáveis")
+    print("2 - Calculadora simples")
     print("")
     opcao = input("Qual resposta quer ver? Digite o numero: ")
     print("")
@@ -17,6 +18,18 @@ while continuar:
             print(f"Nome: {nome}")
             print(f"Idade: {idade} anos")
             print(f"Altura: {altura} metros")
+
+        
+        case "2":
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            print(f"A soma de {num1} e {num2} é: {num1 + num2:.2f}")
+            print(f"A subtração de {num1} e {num2} é: {num1 - num2:.2f}")
+            print(f"A multiplicação de {num1} e {num2} é: {num1 * num2:.2f}")
+            if num2 != 0:
+                print(f"A divisão de {num1} por {num2} é: {num1 / num2:.2f}")
+            else:
+                print("Divisão por zero não é permitida.")
 
         case _:
             print("Opção inválida.")
