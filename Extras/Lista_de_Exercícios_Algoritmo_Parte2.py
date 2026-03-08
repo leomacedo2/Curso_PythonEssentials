60916e49837fa5a5f7286299f112cbac83bf2adf
Leo Macedo <leomacedo19@gmail.com>
2026-03-08 02:05:56 -0300

    feat: Questao 2 da Lista D parte 2

--- Extras/Lista_de_Exercícios_Algoritmo_Parte2.py
+++ Extras/Lista_de_Exercícios_Algoritmo_Parte2.py
@@ -4,6 +4,7 @@ while continuar:
     print("")
     print("Lista de Exercícios Parte 2 - Algoritmo")
     print("1 - Conversão de Dolares para Reais")
+    print("2 - Quanto ganha motorista de uber")
 
     print("")
     opcao = input("Qual resposta quer ver? Digite o numero: ")
@@ -16,6 +17,10 @@ while continuar:
             cotacao_dolar = float(input("Digite a cotação do Dolar atual: "))
             valor_real = valor_dolar * cotacao_dolar
             print(f"O valor em Reais é: R${valor_real:.2f}")
+
+        case "2":
+            viagens = int(input("Digite o número de viagens realizadas hoje: "))
+            print(f"O motorista de Uber ganhou R${viagens * 6.8:.2f} com as viagens realizadas.")
 
         case _:
             print("Opção inválida.")
