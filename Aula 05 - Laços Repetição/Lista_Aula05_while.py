c93cd377eed4e9033a9e1ef2d9cfe867bd1c531a
Leo Macedo <leomacedo19@gmail.com>
2026-03-10 05:24:53 -0300

    feat: Questão 2 da Lista de While.

--- Aula 05 - Laços Repetição/Lista_Aula05_while.py
+++ Aula 05 - Laços Repetição/Lista_Aula05_while.py
@@ -4,6 +4,7 @@ while continuar :
     print("")
     print("Questionário da Aula 5. Laço de repetição 'while'. Escolha uma opção: ")
     print("1 - Ler numeros até o usuario digitar um valor negativo")
+    print("2 - Lendo notas de alunos enquanto ela for valida (entre 0 e 10)")
     print("")
     opcao = input("Qual resposta quer ver? Digite o numero: ")
     print("")
@@ -19,6 +20,15 @@ while continuar :
                 else:
                     print("Valor negativo digitado. Encerrando o programa.")
 
+        case "2":
+            nota = -1
+            while nota < 0 or nota > 10:
+                nota = float(input("Digite uma nota entre 0 e 10: "))
+                if nota < 0 or nota > 10:
+                    print("Nota inválida. Programa encerrado.")
+                else:
+                    print(f"Nota de aluno: {nota:.2f}")
+            
         case _:
             print("Opção invalida!")
     
