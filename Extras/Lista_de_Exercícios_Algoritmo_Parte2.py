113fd67f3542cbd60cf9543e150feeff03b1cc72
Leo Macedo <leomacedo19@gmail.com>
2026-03-17 12:03:59 -0300

    feat: Qiestão 4 e 5 da lista D

--- Extras/Lista_de_Exercícios_Algoritmo_Parte2.py
+++ Extras/Lista_de_Exercícios_Algoritmo_Parte2.py
@@ -6,6 +6,8 @@ while continuar:
     print("1 - Conversão de Dolares para Reais")
     print("2 - Quanto ganha motorista de uber")
     print("3 - Identificar se é par ou impar")
+    print("4 - Ler 10 cidades em uma unica variavel")
+    print("5 - Ler 10 carros, placas e valores de diarias em uma unica variavel")
 
     print("")
     opcao = input("Qual resposta quer ver? Digite o numero: ")
@@ -29,6 +31,23 @@ while continuar:
                 print(f"O valor {num} é par")
             else:
                 print(f"O valor é impar")
+        
+        case "4":
+            cidades = ""
+            for i in range(1, 11):
+              cidades = cidades + input(f"Digite o nome da cidade {i}: ") + ", "
+            print("\nAs cidades informadas foram:")
+            print(cidades)
+
+        case "5":
+            carros = ""
+            for i in range(1, 11):
+                carros = carros + "\n Carro " + input(f"Digite o nome do carro {i}: ") + ", "
+                carros = carros + "Placa: " + input(f"Digite a placa do carro {i}: ") + ", "
+                carros = carros + "Valor: " + "R$ " + str(float(input(f"Digite o valor da diaria do carro {i}: R$ ")))
+            print("\nOs carros lidos foram:")
+            print(carros)
+
 
         case _:
             print("Opção inválida.")
